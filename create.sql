ab504b4630eb9743d7fbdf12a5b1acb53e90c7e7
rphgoossens <r.p.h.goossens@gmail.com>
2019-04-01 22:47:14 +0200

    Changed sequences from table to identity

--- create.sql
+++ create.sql
@@ -1,6 +1,3 @@
-create table beer (id bigint not null, beer_type varchar(255) not null, name varchar(255) not null, brewery_id bigint, primary key (id)) engine=MyISAM
-create table brewery (id bigint not null, country varchar(255), name varchar(255) not null, primary key (id)) engine=MyISAM
-create table hibernate_sequence (next_val bigint) engine=MyISAM
-insert into hibernate_sequence values ( 1 )
-insert into hibernate_sequence values ( 1 )
-alter table beer add constraint FK5p78eficyvoadx99wd9owh0kf foreign key (brewery_id) references brewery (id)
+create table beer (id bigint not null auto_increment, beer_type varchar(255) not null, name varchar(255) not null, brewery_id bigint, primary key (id)) engine=MyISAM;
+create table brewery (id bigint not null auto_increment, country varchar(255), name varchar(255) not null, primary key (id)) engine=MyISAM;
+alter table beer add constraint FK5p78eficyvoadx99wd9owh0kf foreign key (brewery_id) references brewery (id);
--- drop.sql
+++ drop.sql
@@ -1,3 +1,2 @@
-drop table if exists beer
-drop table if exists brewery
-drop table if exists hibernate_sequence
+drop table if exists beer;
+drop table if exists brewery;
